8c7459029503f6efc9a2eb74b976e3bde75959eb
bbqstand <mashrurfaiyaz@iut-dhaka.edu>
2025-03-25 20:43:49 +0600

    Removed more comments from FlightReservation.java and RandomGenerator.java. YUCK!!

--- src/FlightReservation.java
+++ src/FlightReservation.java
@@ -10,24 +10,9 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FlightReservation implements DisplayClass {
-
-    //        ************************************************************ Fields ************************************************************
     Flight flight = new Flight();
     int flightIndexInFlightList;
 
-    //        ************************************************************ Behaviours/Methods ************************************************************
-
-
-    /**
-     * Book the numOfTickets for said flight for the specified user. Update the available seats in main system by
-     * Subtracting the numOfTickets from the main system. If a new customer registers for the flight, then it adds
-     * the customer to that flight, else if the user is already added to that flight, then it just updates the
-     * numOfSeats of that flight.
-     *
-     * @param flightNo     FlightID of the flight to be booked
-     * @param numOfTickets number of tickets to be booked
-     * @param userID       userID of the user which is booking the flight
-     */
     void bookFlight(String flightNo, int numOfTickets, String userID) {
         boolean isFound = false;
         for (Flight f1 : flight.getFlightList()) {
@@ -60,12 +45,6 @@ public class FlightReservation implements DisplayClass {
         }
     }
 
-    /**
-     * Cancels the flight for a particular user and return/add the numOfTickets back to
-     * the main flight scheduler.
-     *
-     * @param userID    ID of the user for whom the flight is to be cancelled
-     */
     void cancelFlight(String userID) {
         String flightNum = "";
         Scanner read = new Scanner(System.in);
@@ -151,7 +130,6 @@ public class FlightReservation implements DisplayClass {
         }
     }
 
-    /*toString() Method for displaying number of flights registered by single user...*/
     public String toString(int serialNum, Flight flights, Customer customer) {
         return String.format("| %-5d| %-41s | %-9s | \t%-9d | %-21s | %-22s | %-10s  |   %-6sHrs |  %-4s  | %-10s |", serialNum, flights.getFlightSchedule(), flights.getFlightNumber(), customer.numOfTicketsBookedByUser.get(serialNum - 1), flights.getFromWhichCity(), flights.getToWhichCity(), flights.fetchArrivalTime(), flights.getFlightTime(), flights.getGate(), flightStatus(flights));
     }
@@ -173,8 +151,6 @@ public class FlightReservation implements DisplayClass {
             }
         }
     }
-
-    /*overloaded toString() method for displaying all users in a flight....*/
 
     public String toString(int serialNum, Customer customer, int index) {
         return String.format("%10s| %-10d | %-10s | %-32s | %-7s | %-27s | %-35s | %-23s |       %-7s  |", "", (serialNum + 1), customer.randomIDDisplay(customer.getUserID()), customer.getName(),
