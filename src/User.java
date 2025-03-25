6a8d4725081d97a7a1b8291343705199a2b0385b
bbqstand <mashrurfaiyaz@iut-dhaka.edu>
2025-03-25 20:46:19 +0600

    my desire to work in the industry has significantly decreased seeing the amount of comments i have had to remove. this time it was from RolesAndPermissions.java and User.java

--- src/User.java
+++ src/User.java
@@ -10,19 +10,8 @@ import java.util.Scanner;
 
 public class User {
 
-    // ************************************************************ Fields
-    // ************************************************************
-
-    /*
-     * 2D Array to store admin credentials. Default credentials are stored on [0][0]
-     * index. Max num of admins can be 10....
-     */
     static String[][] adminUserNameAndPassword = new String[10][2];
     private static List<Customer> customersCollection = new ArrayList<>();
-
-    // ************************************************************
-    // Behaviours/Methods
-    // ************************************************************
 
     public static void main(String[] args) {
         int countNumOfUsers = 1;
@@ -47,17 +36,9 @@ public class User {
 
         do {
             Scanner read1 = new Scanner(System.in);
-            /*
-             * If desiredOption is 1 then call the login method.... if default credentials
-             * are used then set the permission
-             * level to standard/default where the user can just view the customer's
-             * data...if not found, then return -1, and if
-             * data is found then show the user display menu for adding, updating, deleting
-             * and searching users/customers...
-             */
+
             if (desiredOption == 1) {
 
-                /* Default username and password.... */
                 adminUserNameAndPassword[0][0] = "root";
                 adminUserNameAndPassword[0][1] = "root";
                 
@@ -67,7 +48,6 @@ public class User {
                 String password = read1.nextLine();
                 System.out.println();
 
-                /* Checking the RolesAndPermissions...... */
                 if (r1.isPrivilegedUserOrNot(username, password) == -1) {
                     System.out.printf(
                             "\n%20sERROR!!! Unable to login Cannot find user with the entered credentials.... Try Creating New Credentials or get yourself register by pressing 4....\n",
@@ -82,11 +62,6 @@ public class User {
                             "%-20sLogged in Successfully as \"%s\"..... For further Proceedings, enter a value from below....",
                             "", username);
 
-                    /*
-                     * Going to Display the CRUD operations to be performed by the privileged
-                     * user.....Which includes Creating, Updating
-                     * Reading(Searching) and deleting a customer....
-                     */
                     do {
                         System.out.printf("\n\n%-60s+++++++++ 2nd Layer Menu +++++++++%50sLogged in as \"%s\"\n", "",
                                 "", username);
@@ -103,28 +78,19 @@ public class User {
                         System.out.printf("%-30s (i) Enter 0 to Go back to the Main Menu/Logout....\n", "");
                         System.out.print("Enter the desired Choice :   ");
                         desiredOption = read.nextInt();
-                        /* If 1 is entered by the privileged user, then add a new customer...... */
+
                         if (desiredOption == 1) {
                            
                             c1.addNewCustomer();
                         } else if (desiredOption == 2) {
-                            /*
-                             * If 2 is entered by the privileged user, then call the search method of the
-                             * Customer class
-                             */
-                            
+
                             c1.displayCustomersData(false);
                             System.out.print("Enter the CustomerID to Search :\t");
                             String customerID = read1.nextLine();
                             System.out.println();
                             c1.searchUser(customerID);
                         } else if (desiredOption == 3) {
-                            /*
-                             * If 3 is entered by the user, then call the update method of the Customer
-                             * Class with required
-                             * arguments.....
-                             */
-                            
+
                             c1.displayCustomersData(false);
                             System.out.print("Enter the CustomerID to Update its Data :\t");
                             String customerID = read1.nextLine();
@@ -135,10 +101,7 @@ public class User {
                             }
 
                         } else if (desiredOption == 4) {
-                            /*
-                             * If 4 is entered, then ask the user to enter the customer id, and then delete
-                             * that customer....
-                             */
+
                             c1.displayCustomersData(false);
                             System.out.print("Enter the CustomerID to Delete its Data :\t");
                             String customerID = read1.nextLine();
@@ -148,7 +111,7 @@ public class User {
                                 System.out.printf("%-50sNo Customer with the ID %s Found...!!!\n", " ", customerID);
                             }
                         } else if (desiredOption == 5) {
-                            /* Call the Display Method of Customer Class.... */
+
                             c1.displayCustomersData(false);
                         } else if (desiredOption == 6) {
                             c1.displayCustomersData(false);
@@ -194,10 +157,7 @@ public class User {
 
                 }
             } else if (desiredOption == 2) {
-                /*
-                 * If desiredOption is 2, then call the registration method to register a
-                 * user......
-                 */
+
                 System.out.print("\nEnter the UserName to Register :    ");
                 String username = read1.nextLine();
                 System.out.print("Enter the Password to Register :     ");
@@ -209,11 +169,9 @@ public class User {
                     password = read1.nextLine();
                 }
 
-                /* Setting the credentials entered by the user..... */
                 adminUserNameAndPassword[countNumOfUsers][0] = username;
                 adminUserNameAndPassword[countNumOfUsers][1] = password;
 
-                /* Incrementing the numOfUsers */
                 countNumOfUsers++;
             } else if (desiredOption == 3) {
                 System.out.print("\n\nEnter the Email to Login : \t");
@@ -241,7 +199,6 @@ public class User {
                         System.out.print("Enter the desired Choice :   ");
                         desiredChoice = read.nextInt();
                         if (desiredChoice == 1) {
-                            // bookingAndReserving.displayArtWork(1);
                             f1.displayFlightSchedule();
                             System.out.print("\nEnter the desired flight number to book :\t ");
                             String flightToBeBooked = read1.nextLine();
@@ -376,9 +333,6 @@ public class User {
                     "(12) Pressing \"0\" will make you logout of the program...You can login back at anytime with your credentials...for this particular run-time... \n");
         }
     }
-
-    // ************************************************************ Setters &
-    // Getters ************************************************************
 
     public static List<Customer> getCustomersCollection() {
         return customersCollection;
